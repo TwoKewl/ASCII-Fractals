62c6b6721bf9d2a24526176a7ebf9f274a3be18d
TwoKewl <toby@bigdevelopments.co.uk>
2024-11-09 19:12:51 +0000

    Added ASCII Fractal Renderer

---
+++ AsciiFractals/ConsoleColour.cs
@@ -0,0 +1,28 @@
+﻿
+namespace AsciiFractals;
+
+internal struct ConsoleColour()
+{
+    public static int[] Colours = [0, 30, 31, 32, 33, 34, 35, 36, 37];
+
+    public static string GetColour(double u)
+    {
+        if (u < 0.2) return "\x1b[30m";
+        double offset = 1;
+        double r = Math.Sin((u + offset) * 5) / 2 + 0.5;
+        double g = Math.Sin((u + offset) * 5 + 1) / 2 + 0.5;
+        double b = Math.Sin((u + offset) * 5 + 2) / 2 + 0.5;
+
+        return $"\x1b[38;2;{(int)(r * 255)};{(int)(g * 255)};{(int)(b * 255)}m";
+    }
+
+    public static string Reset = "\x1b[0m";
+    public static string Black = "\x1b[30m";
+    public static string Red = "\x1b[31m";
+    public static string Green = "\x1b[32m";
+    public static string Yellow = "\x1b[33m";
+    public static string Blue = "\x1b[34m";
+    public static string Magenta = "\x1b[35m";
+    public static string Cyan = "\x1b[36m";
+    public static string White = "\x1b[37m";
+}
---
+++ AsciiFractals/Program.cs
@@ -0,0 +1,98 @@
+﻿
+using System.Diagnostics;
+using System.Text;
+
+namespace AsciiFractals;
+
+internal class Program
+{
+    private double xMin = -2.5 * 4;
+    private double xMax = 1.0 * 4;
+    private double yMin = -1.0 * 4;
+    private double yMax = 1.0 * 4;
+    private readonly int _maxIters = 25000;
+	private readonly double zoomCenterX = -1.7499386236239305;
+    private readonly double zoomCenterY = -0.0000064245648392358135;
+
+    private readonly string _chars = " `.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@";
+    // private readonly string _chars = " ░▒▓";
+
+	static void Main()
+	{
+        Console.WriteLine("Press any key to start!");
+		Console.ReadKey(true);
+		Console.CursorVisible = false;
+		string[,] charList = new string[Console.WindowWidth * 2, Console.WindowHeight];
+        StringBuilder sb = new();
+		Program p = new();
+		Stopwatch sw = Stopwatch.StartNew();
+
+		// Console.Write("\x1b[1m"); // Make everything bold (optional)
+
+		while (true)
+		{
+			sw.Restart();
+
+			int width = Console.WindowWidth;
+			int height = Console.WindowHeight;
+
+			Parallel.For(0, width * height, i =>
+			{
+				int x = i % width;
+				int y = i / width;
+				
+                (string, char) c = p.GetCharFromCoord(x, y, width, height);
+                charList[x * 2, y] = c.Item1;
+                charList[x * 2 + 1, y] = c.Item2.ToString();
+			});
+
+			sb.Clear();
+
+			for (int j = 0; j < charList.GetLength(1); j++)
+			{
+				for (int i = 0; i < charList.GetLength(0); i++)
+				{
+					sb.Append(charList[i, j]);
+				}
+
+				if (j != height - 1) sb.AppendLine();
+			}
+
+			Console.SetCursorPosition(0, 0);
+			Console.Write(sb);
+			while (sw.ElapsedMilliseconds <= (1000f / 10f)) { }
+
+			double newWid = (p.xMax - p.xMin) / 1.02;
+			double newHei = (p.yMax - p.yMin) / 1.02;
+
+            p.xMin = p.zoomCenterX - newWid / 2;
+            p.xMax = p.zoomCenterX + newWid / 2;
+            p.yMin = p.zoomCenterY - newHei / 2;
+            p.yMax = p.zoomCenterY + newHei / 2;
+        }
+	}
+
+    public (string, char) GetCharFromCoord(int x, int y, int width, int height)
+    {
+        double u = (double)x / width;
+        double v = (double)y / height;
+        double cx = xMin + u * (xMax - xMin);
+        double cy = yMin + v * (yMax - yMin);
+        double zx = 0;
+        double zy = 0;
+
+        for (int i = 0; i < _maxIters; i++)
+        {
+            (zx, zy) = (zx * zx - zy * zy + cx, 2 * zx * zy + cy);
+
+            if (zx * zx + zy * zy > 1024)
+            {
+                double ratio = (double)i / _maxIters;
+                int charIndex = (int)(ratio * (_chars.Length - 1));
+                return (ConsoleColour.GetColour(ratio), _chars[charIndex]);
+            }
+        }
+
+        return (ConsoleColour.GetColour(1), _chars[^1]);
+    }
+}
